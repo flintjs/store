25a64fa21967f24067670b06b9decba25ec20023
natew <natewienert@gmail.com>
2016-01-12 16:23:40 -0800

    tweaks

--- link.js
+++ link.js
@@ -4,7 +4,7 @@ view Link {
   prop icon:? string
   prop label: string
 
-  <a {...view.props} class={{ active }}>
+  <a root yield class={{ active }}>
     <FontIcon if={icon} class="icon" value={icon} />
     <abbr if={label}>{label}</abbr>
     <small if={count && parseInt(count) !== 0}>{count}</small>
